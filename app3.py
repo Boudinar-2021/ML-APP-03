d427814b39486eeb646b4859b8dd915da625eb65
Boudinar Mohamed El Amine <83282719+Boudinar-2021@users.noreply.github.com>
2024-09-09 08:06:15 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -7,9 +7,9 @@ from nltk.corpus import stopwords
 
 nltk.download('stopwords')
 
-def clean_text(text, keep_emojis, keep_stopwords, keep_punctuation, language):
+def clean_text(text, keep_emojis, keep_stopwords, keep_punctuation, keep_digits, language):
     if not keep_emojis:
-        text = emoji.replace_emoji(text, replace='')  # Remove emojis
+        text = emoji.replace_emoji(text, replace='')  
 
     arabic_punctuation = '؟،؛«»'
     if not keep_punctuation:
@@ -17,6 +17,9 @@ def clean_text(text, keep_emojis, keep_stopwords, keep_punctuation, language):
             text = text.translate(str.maketrans('', '', arabic_punctuation + string.punctuation))
         else:
             text = text.translate(str.maketrans('', '', string.punctuation))
+
+    if not keep_digits:
+        text = ''.join([char for char in text if not char.isdigit()])
 
     if not keep_stopwords:
         stop_words = set(stopwords.words(language))
@@ -35,6 +38,10 @@ def detect_punctuation(text):
 def detect_emojis(text):
     emojis_in_text = [char for char in text if char in emoji.EMOJI_DATA]
     return emojis_in_text
+
+def detect_digits(text):
+    digits_in_text = [char for char in text if char.isdigit()]
+    return digits_in_text
 
 st.set_page_config(page_title='Text Cleaner App', layout='wide')
 
@@ -62,6 +69,7 @@ language = language_map[language_choice]
 keep_emojis = st.sidebar.radio("Keep the emojis?", ('Yes', 'No')) == 'Yes'
 keep_stopwords = st.sidebar.radio("Keep the stopwords?", ('Yes', 'No')) == 'Yes'
 keep_punctuation = st.sidebar.radio("Keep the punctuations?", ('Yes', 'No')) == 'Yes'
+keep_digits = st.sidebar.radio("Keep the digits?", ('Yes', 'No')) == 'Yes'
 
 st.markdown('<h2 class="centered-text">Input Text</h2>', unsafe_allow_html=True)
 text = st.text_area("Write your text here:", height=200)
@@ -80,6 +88,7 @@ if st.session_state.clean_text_button_clicked and text:
     stopwords_in_text = detect_stopwords(text, stop_words)
     punctuation_in_text = detect_punctuation(text)
     emojis_in_text = detect_emojis(text)
+    digits_in_text = detect_digits(text)
 
     if keep_stopwords and stopwords_in_text:
         st.warning(f"Stop words detected: {', '.join(stopwords_in_text)}")
@@ -87,8 +96,10 @@ if st.session_state.clean_text_button_clicked and text:
         st.warning(f"Punctuation marks detected: {', '.join(punctuation_in_text)}")
     if keep_emojis and emojis_in_text:
         st.warning(f"Emojis detected: {', '.join(emojis_in_text)}")
+    if keep_digits and digits_in_text:
+        st.warning(f"Digits detected: {', '.join(digits_in_text)}")
 
-    cleaned_text = clean_text(text, keep_emojis, keep_stopwords, keep_punctuation, language)
+    cleaned_text = clean_text(text, keep_emojis, keep_stopwords, keep_punctuation, keep_digits, language)
 
     st.markdown('<h2 class="centered-text">Cleaned Text</h2>', unsafe_allow_html=True)
     st.markdown(f'<div class="cleaned-text">{cleaned_text}</div>', unsafe_allow_html=True)
